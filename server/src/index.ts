d13b13690e0abf10605324f9edd6fd3fc30dce5f
jrquadros <jrmquadros@gmail.com>
2020-04-26 04:55:19 -0300

    feat(server): db connection setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import { App } from './App'
+import * as mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
@@ -6,5 +7,17 @@ dotenv.config()
 const PORT = process.env.SERVER_PORT
 
 ;(async function start() {
+  mongoose
+    .connect('mongodb://localhost:27017/todo', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('database connected')
+    })
+    .catch((error) => {
+      console.log(`database error ${error}`)
+    })
+
   App.listen(PORT, () => console.log(`Server is running at ${PORT}`))
 })()
