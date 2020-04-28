3a54d90afd5ca372c99847af0182aad987d4c5e0
jrquadros <jrmquadros@gmail.com>
2020-04-28 03:23:16 -0300

    feat(db-setup): db connection

--- auth-server/db/db.go
+++ auth-server/db/db.go
@@ -1,11 +1,27 @@
 package db
 
 import (
+	"context"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GetDBCollection() {
+func GetDBCollection() (*mongo.Collection, error) {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	collection := client.Database("blog").Collection("users")
+
+	return collection, nil
 
 }
