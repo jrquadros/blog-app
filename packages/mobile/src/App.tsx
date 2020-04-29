7b8fdb6580bb03078df5a2c1cfa09ca3167a09c4
jrquadros <jrmquadros@gmail.com>
2020-04-29 06:30:41 -0300

    feat(app): add signin route

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { View, Text } from 'react-native'
+import { SignIn } from './screens/SignIn'
 
 const Stack = createStackNavigator()
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>lkfjalkdfjlakdjflakdjf</Text>
-      </View>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
