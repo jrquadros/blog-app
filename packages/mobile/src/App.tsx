248f204036f6b46fa6b178bad23f61b2b75e6ccb
jrquadros <jrmquadros@gmail.com>
2020-04-28 22:38:39 -0300

    chore(prettier): edit prettierrc file

---
+++ packages/mobile/src/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react'
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native'
+
+// declare const global: { HermesInternal: null | {} }
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <Text>App</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
+
+export default App
