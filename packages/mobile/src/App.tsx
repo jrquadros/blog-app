9ce6345b6cb3adbd389ef3d1a1329f1e4c8e6b01
jrquadros <jrmquadros@gmail.com>
2020-04-30 03:21:21 -0300

    chore(app): add routes entries

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -1,16 +1,21 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { SignIn } from './screens/SignIn'
 
-const Stack = createStackNavigator()
+type RootStackParamList = {
+  SignIn: undefined
+}
+
+const RootStack = createStackNavigator<RootStackParamList>()
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn">
-        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
-      </Stack.Navigator>
+      <RootStack.Navigator initialRouteName="SignIn">
+        <RootStack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+      </RootStack.Navigator>
     </NavigationContainer>
   )
 }
