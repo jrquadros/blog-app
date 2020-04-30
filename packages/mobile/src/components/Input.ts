09d9a095f63bb5c5522311b7b8bc61fc21d6f313
jrquadros <jrmquadros@gmail.com>
2020-04-30 06:42:49 -0300

    chore(components): add input props

--- packages/mobile/src/components/Input.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native'
-
-export const Input = styled.TextInput`
-  padding: 15px;
-  background-color: #fff;
-  align-self: stretch;
-  margin-bottom: 5px;
-  font-size: 14px;
-`
