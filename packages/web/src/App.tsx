b00a0a4c7baf37f6510bb0c6328a39e7cfe055b2
jrquadros <jrmquadros@gmail.com>
2020-05-04 13:43:12 -0300

    fix(deps): add react and react-dom to nohoist

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: tomato;
+`
 
 function App() {
   return (
@@ -10,17 +15,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Learn React</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
