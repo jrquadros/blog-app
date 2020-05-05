6566b7df394c1f69f3298f180ae8afc9a7e72bfe
jrquadros <jrmquadros@gmail.com>
2020-05-05 18:14:59 -0300

    chore(app): remove unused vars

--- packages/web/src/pages/CreatePost.tsx
+++ packages/web/src/pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Header } from '../components/Header'
 import { Button } from '../components/Button'
