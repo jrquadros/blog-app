fe4b6e55afb8c36743971a5731283abbe12d42fe
jrquadros <jrmquadros@gmail.com>
2020-05-05 17:29:34 -0300

    chore(components): add add-post-button component

--- packages/web/src/components/AddPostButton.tsx
+++ packages/web/src/components/AddPostButton.tsx
@@ -2,13 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 import { PencilAlt } from '@styled-icons/fa-solid/PencilAlt'
 
+interface IAddPostButton {
+  onClick: () => void
+}
+
 const PencilIcon = styled(PencilAlt)`
   width: 1rem;
   color: #fff;
 `
 
 const Wrapper = styled.div`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
   padding: 1rem;
   border-radius: 50%;
   position: fixed;
+  background-color: tomato;
+
+  :hover {
+    cursor: pointer;
+  }
 `
+
+export const AddPostButton = ({ onClick }: IAddPostButton) => {
+  return (
+    <Wrapper onClick={onClick}>
+      <PencilIcon />
+    </Wrapper>
+  )
+}
