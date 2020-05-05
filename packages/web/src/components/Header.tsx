fe580bb997344186bfe718c3ea9a6063961575f1
jrquadros <jrmquadros@gmail.com>
2020-05-05 10:42:10 -0300

    feat(home): add logout

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 
 interface IHeaderProps {
   title?: string
+  items?: IItem[]
+}
+
+interface IItem {
+  onClick?: () => void
+  label: string
 }
 
 const Wrapper = styled.header`
@@ -10,21 +16,35 @@ const Wrapper = styled.header`
   width: 100%;
   background-color: #fff;
   display: flex;
+  justify-content: space-between;
   padding: 1rem;
   border-bottom: 1px solid #f5f5f5;
-  justify-content: center;
 `
 
 const Title = styled.span`
   font-size: 1.2rem;
   color: #555;
   font-weight: 500;
+  align-self: center;
 `
 
-export const Header = ({ title }: IHeaderProps) => {
+const Item = styled.span`
+  font-size: 1rem;
+  color: #555;
+  font-weight: 500;
+  justify-self: flex-start;
+  :hover {
+    cursor: pointer;
+  }
+`
+
+export const Header = ({ title, items }: IHeaderProps) => {
   return (
     <Wrapper>
       <Title>{title}</Title>
+      {items?.map((item) => (
+        <Item onClick={item.onClick}>{item.label}</Item>
+      ))}
     </Wrapper>
   )
 }
