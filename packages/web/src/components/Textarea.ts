78c660982007a4e8f0d9dab015400b071169dc79
jrquadros <jrmquadros@gmail.com>
2020-05-05 17:26:32 -0300

    chore(components): add textarea component

---
+++ packages/web/src/components/Textarea.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Textarea = styled.textarea`
+  background-color: #fff;
+  border: none;
+  border-radius: 0.3rem;
+  font-size: 1rem;
+  padding: 0.8rem;
+  align-self: stretch;
+  resize: none;
+`
